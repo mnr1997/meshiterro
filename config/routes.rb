a5041431ef3fb9f12b1e1feb4968f361039d0d02
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-04-17 14:42:27 +0000

    [finish]4-18

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,9 @@ Rails.application.routes.draw do
   root to: "homes#top"
   get 'homes/about' => "homes#about", as: 'about'
 
-  resources :post_images, only: [:new, :create, :index, :show, :destroy]
+  resources :post_images, only: [:new, :create, :index, :show, :destroy] do
+    resources :post_comments, only: [:create, :destroy]
+  end
   resources :users, only: [:show, :edit, :update]
 
 end
