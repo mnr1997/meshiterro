3708e6aff27e952f587e2b041deca1494f31a2df
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-04-17 11:55:53 +0000

    [finish]4-15

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: "homes#top"
   get 'homes/about' => "homes#about", as: 'about'
-  
+
   resources :post_images, only: [:new, :create, :index, :show, :destroy]
   resources :users, only: [:show, :edit]
-  
+
 end
