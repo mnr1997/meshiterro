6b12546899e0f959d6a7b012dcd3116fde994239
mnr1997 <161993650+mnr1997@users.noreply.github.com>
2024-05-08 17:03:35 +0000

    admin[finish]

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,5 +1,5 @@
 class Admin::UsersController < ApplicationController
-  before_action :authenticate_admin!
+  
   def destroy
     @user = User.find(params[:id])
     @user.destroy
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,18 +1,22 @@
 class ApplicationController < ActionController::Base
-  before_action :authenticate_user!, except: [:top], unless: :admin_controller?
-  before_action :configure_permitted_parameters, if: :devise_controller?
+  before_action :configure_authentication
   
-  def after_sign_in_path_for(resource)
-    post_images_path
+  private
+  
+  def configure_authentication
+    if admin_controller?
+      authenticate_admin!
+    else
+      authenticate_user! unless action_is_public?
+    end
   end
   
-  def after_sign_out_path_for(resource)
-    about_path
+  def admin_controller?
+    self.class.module_parent_name == 'Admin'
   end
   
-  protected
-  
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  def action_is_public?
+    controller_name == 'homes' && action_name == 'top'
   end
+  
 end
